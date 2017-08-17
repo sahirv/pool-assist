ed41e121e2be0c01d521f32879726110f0a5d785
Sahir Vellani <sahir.vellani@gmail.com>
2017-08-16 23:07:11 -0400

    fetch receive image with server

--- circle_detection.py
+++ circle_detection.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('training_sets/cue_ball/cb_green_1.png', 1)
-
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def detectCircle():
+    img = cv2.imread('training_sets/cue_ball/cb_green_1.png', 1)
+    filename = 'img.png'
+    cv2.imwrite(filename, img)
+    return filename
---
+++ server.py
@@ -0,0 +1,39 @@
+import circle_detection as bd
+import os
+
+from flask import Flask
+from flask import send_file
+from flask import g
+
+app = Flask(__name__)
+
+
+# decorator to run a function after ball detection response is formed
+def after_bd_response(fn):
+    if not hasattr(g, 'call_after_bd_response'):
+        g.call_after_bd_response = []
+    # store function to be run in g object
+    g.call_after_bd_response.append(fn)
+    return fn
+
+
+# finds decorated functions and runs them
+@app.after_request
+def per_request_callback(res):
+    # looks in g object
+    for fn in getattr(g, 'call_after_bd_response', ()):
+        fn()
+    return res
+
+
+# handler for requests to ball detection (bd)
+@app.route("/bd")
+def ballDetection():
+    filename = bd.detectCircle()
+
+    # image deletion after response is sent
+    @after_bd_response
+    def delete_image():
+        os.remove(filename)
+
+    return send_file(filename, mimetype='image/gif')
