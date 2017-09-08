b1341d8f093c26b093735dabc7d37575d2eff50b
Sahir Vellani <sahir.vellani@gmail.com>
2017-09-08 00:23:13 -0400

    clean up code

--- circle_detection.py
+++ circle_detection.py
@@ -6,7 +6,7 @@ from ball_classifier import classifyBall
 
 def findCircles():
     # read image - 0 is greyscale, 1 - color
-    table_img = cv2.imread('training_sets/tables/example_table.png', 1)
+    table_img = cv2.imread('training_sets/tables/extable6.png', 1)
     table_img_col = table_img.copy()
     table_img_grey = cv2.cvtColor(table_img, cv2.COLOR_BGR2GRAY)
     table_orig = table_img_grey.copy()
@@ -21,7 +21,7 @@ def findCircles():
     # Perform Hough circle transform
     circles = cv2.HoughCircles(table_canny, cv2.HOUGH_GRADIENT, 1, 25, param1=90, param2=30, maxRadius=50, minRadius=14)
 
-    avgRadius = 0
+    avgObjRadius = 0
     stripes = []
     solids = []
     cueBall = (0,0)
@@ -29,18 +29,20 @@ def findCircles():
     if circles is not None:
         print("Found circles")
         circles = np.round(circles[0, :]).astype("int")
-        tempRadius = 0
+        totAvgRadius = sum(i[2] for i in circles) // len(circles)
+        objBallCounter = 0
         for x, y, r in circles:
-            if r <= 30:
-                tempRadius += r
-        avgRadius = tempRadius // 14
+            if r <= totAvgRadius:
+                objBallCounter += 1
+                avgObjRadius += r
+        avgObjRadius = avgObjRadius // objBallCounter
         for x, y, r in circles:
             if r > 30:
                 pockets.append([x, y, r])
                 cv2.circle(table_img, (x, y), r, (0, 210, 30), 3)
             else:
                 # store pixels within circle below
-                ball = isolateBall(x, y, avgRadius, table_img)
+                ball = isolateBall(x, y, avgObjRadius, table_img)
                 ballType = classifyBall(ball)
                 if ballType == "stripe":
                     stripes.append((x, y))
@@ -50,7 +52,7 @@ def findCircles():
                     cueBall = (x, y)
                 else:
                     raise Exception("Ball can not be classified. X= " + x + " Y= " + y)
-                cv2.circle(table_img, (x, y), avgRadius, (150, 100, 255), 4)
+                cv2.circle(table_img, (x, y), avgObjRadius, (150, 100, 255), 4)
 
     #concatenate before+after images
 
@@ -62,10 +64,8 @@ def findCircles():
 
 def isolateBall(x, y, r, img):
     height, width = img.shape[:2]
-    print(len(img[0]))
     mask = np.zeros((height, width, 3), np.uint8)
     height1, width1 = mask.shape[:2]
-    print(len(mask[0]))
     ball = []
     for i in range(x - r, x + r): # columns (width)
         for j in range(y - r, y + r): # rows (height)
